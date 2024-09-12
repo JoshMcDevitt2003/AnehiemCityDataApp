e575f2e45d390aec9914d7fcebbd225e40696f27
Joshua McDevitt <jmcdevitt2003@gmail.com>
2024-09-12 13:13:50 -0700

    apifix

--- src/Page6.js
+++ src/Page6.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 
 function Page6() {
     const[message, setMessage] = useState('')
     const[file,setFile] = useState(null)
+    
+    const handleChange = (event) => {
+        setFile(event.target.files[0]);
+    }
     const handleSubmit = async (event) => {
         event.preventDefault();
-        setFile(event.target.files[0]);
         if (!file) {
             setMessage('Please upload File')
         }
@@ -15,7 +17,7 @@ function Page6() {
         formData.append('file', file)
 
         try {
-            const response = await fetch(' https://6hynxwo06g.execute-api.us-east-1.amazonaws.com/fileupload', {
+            const response = await fetch('https://6hynxwo06g.execute-api.us-east-1.amazonaws.com/fileupload', {
                 method: 'POST',
                 body: formData
             })
@@ -56,8 +58,8 @@ function Page6() {
             Upload your data set on this Page! Make sure it is in .csv format and aligns with the City of Anaheim's data set format.
             </p>
             <form onSubmit={handleSubmit}>
-                <label for="file">Choose CSV file:</label>
-                <input id="filebutton" type="file"  name="file" accept=".csv" required />
+                <label htmlFor="file">Choose CSV file:</label>
+                <input id="filebutton" type="file"  name="file" accept=".csv"  onChange={handleChange} required />
                 <button type="submit">Upload</button>
             </form>
             <p>{message}</p>
